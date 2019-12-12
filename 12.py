a24ecf804d258d62ba1df1279e919860ddce7a25
Ricardo Azpeitia <razpeitia@gmail.com>
2019-12-12 00:24:17 -0800

    Day 12; Part 2

--- 12.py
+++ 12.py
@@ -1,5 +1,6 @@
 from itertools import combinations
 from copy import copy
+from math import gcd
 
 def calc(x1, x2):
     if x1 > x2:
@@ -28,8 +29,14 @@ def main():
         moons = [[a for a in line.strip().translate(table).split(",")] for line in f]
         moons = [[i, int(x), int(y), int(z)] for (i, (x, y, z)) in enumerate(moons, start=0)]
     speeds = [[0] * 3 for _ in moons]
-    for i in range(1000+1):
-        print_all(i, moons, speeds)
+    i = 0
+    T1 = [moon[1] for moon in moons]
+    T2 = [moon[2] for moon in moons]
+    T3 = [moon[3] for moon in moons]
+    T4 = [0 for _ in moons]
+    X = [0] * 3
+    while True:
+        #print_all(i, moons, speeds)
         for (id1, x1, y1, z1), (id2, x2, y2, z2) in combinations(moons, 2):
             d1, d2 = calc(x1, x2)
             speeds[id1][0] += d1
@@ -41,6 +48,31 @@ def main():
             speeds[id1][2] += d1
             speeds[id2][2] += d2
         moons = [[id, x+speeds[id][0], y+speeds[id][1], z+speeds[id][2]] for (id, x, y, z) in moons]
+        
+        t1 = [moon[1] for moon in moons]
+        t2 = [moon[2] for moon in moons]
+        t3 = [moon[3] for moon in moons]
+        t4 = [speed[0] for speed in speeds]
+        t5 = [speed[1] for speed in speeds]
+        t6 = [speed[2] for speed in speeds]
+        
+        if X[0] == 0 and t1 == T1 and t4 == T4:
+            X[0] = i+1
+            print("x=", i+1)
+        if X[1] == 0 and t2 == T2 and t5 == T4:
+            X[1] = i+1
+            print("y=", i+1)
+        if X[2] == 0 and t3 == T3 and t6 == T4:
+            X[2] = i+1
+            print("z=", i+1)
+        i += 1
+        if all(X):
+            break
         #input("Press enter...")
+    print(X)
+    lcm = X[0]
+    for i in X[1:]:
+      lcm = lcm*i//gcd(lcm, i)
+    print(lcm)
 
 main()
