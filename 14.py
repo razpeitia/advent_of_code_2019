8e0315c5b8e5438d8f616ee16def656d003bf8e7
Ricardo Azpeitia Pimentel <ricardo.azpeitia@nextroll.com>
2019-12-25 16:11:54 -0600

    Day 14; Part 2

--- 14.py
+++ 14.py
@@ -1,3 +1,6 @@
+import bisect
+
+
 def parse_step(step):
     mult, product = step.strip().split()
     return int(mult), product
@@ -25,6 +28,17 @@ def resolve(index, need, have):
     return need['ORE']
 
 
+class Arr:
+    def __init__(self, index):
+        self.__index = index
+
+    def __len__(self):
+        return 10**12
+
+    def __getitem__(self, item):
+        return resolve(self.__index, {"FUEL": item}, {})
+
+
 def main():
     with open("input14.txt") as f:
         index = {}
@@ -34,8 +48,13 @@ def main():
             mult, product = parse_step(produce)
             assert product not in index, f"Expected only 1 product {product}"
             index[product] = (int(mult), parse_steps(steps))
-    ans = resolve(index, {"FUEL": 1}, {})
-    print(ans)
+
+    arr = Arr(index)
+    fuel = bisect.bisect_left(arr, 10**12)
+    ore = resolve(index, {"FUEL": fuel - 1}, {})
+    print(fuel - 1, ore)
+    ore = resolve(index, {"FUEL": fuel}, {})
+    print(fuel, ore)
 
 
 main()
