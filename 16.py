3767f257df4f9500dd73e017ddc16584b6b69a12
razpeitia <razpeitia@gmail.com>
2019-12-26 01:06:02 -0600

    Day 16; Part 2

--- 16.py
+++ 16.py
@@ -1,26 +1,20 @@
-import time
-import math
-import numpy as np
-
-
 def main():
-    vfunc = np.vectorize(lambda t: abs(t) % 10)
     with open("input16.txt") as f:
         data = [int(x) for x in f.read().strip()]
-
-    data = np.array(data, np.int64)
+    data = data * 10000
     n = len(data)
-    x = []
-    m = np.array([0, 1, 0, -1], np.int64)
-    for i in range(1, n+1):
-        y = np.repeat(m, i)
-        y = np.tile(y, math.ceil(n / len(y) + 1))
-        y = y[1:n+1]
-        x.append(y)
-    x = np.array(x, np.int64)
+    offset = int(''.join(map(str, data[:7])))
+    print("offset", offset)
+
     for t in range(100):
-        z = vfunc(np.dot(x, data))
-        data = z
-    print(data[:8])
+        output = [0] * n
+        s = 0
+        for i in range(n-1, n // 2, -1):
+            s += data[i]
+            output[i] = s % 10
+        x = output[offset:offset+8]
+        x = ''.join(map(str, x))
+        print(f"{t+1} -> {x}")
+        data = output
 
 main()
